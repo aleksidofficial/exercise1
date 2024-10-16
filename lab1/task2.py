5fec3db2941c122fe39db53960c79cf7ec235e7a
aleksidofficial <118462931+aleksidofficial@users.noreply.github.com>
2024-10-16 10:41:38 +0500

    обновил task2.py

--- lab1/task2.py
+++ lab1/task2.py
@@ -3,7 +3,6 @@ from math import cos, log, sin, exp
 x = float(input("Введите значение переменной x: "))
 y = float(input("Введите значение переменной y: "))
 z = float(input("Введите значение переменной z: "))
-
 a=((x**3)/2)**0.5-sin(y)
 b=((exp(1)**2)/3)-cos(y)+z+log(y)
 
